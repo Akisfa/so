7eb61bd96ad2ea418f69df955674e14bd4d654fe
Akisfa <64148918+Akisfa@users.noreply.github.com>
2021-09-02 15:57:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,4 +4,33 @@ var podaci = [
     "Ucionica: 22",
     "Trajanje Casova: 30 min"
 ];
+
+
+
+
+
+
+
+
+
+
 console.log(podaci);
+var snapshot = {
+    "Snapshot":"Nodes",
+    "Nodes":"Files",
+    "Screenshot":"DLL",
+    "Github":"https://github.com/",
+    "Copyright":"2021",
+    "Done":"19.06.2021",
+    "Programming":"Dimirosky",
+    "Language":"HTML, CSS, JS",
+    "Copy":"Github DLL© 2021",
+    "Got by":"Johnny the great",
+    "Credits to":"akisfa",
+    "Started":"31.08.2021",
+    "Engineers":"akisfa(Dimirosky)",
+    "Site support main develop":"Github&Snapshot",
+    "Organised site support":"akisfa(Dimirosky)"
+
+}
+document.getElementById("snapshot").innerHTML = JSON.stringify(snapshot);
